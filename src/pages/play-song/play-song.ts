0cf9f064c886c176b8618269d2c5abeaf103304a
Alma G <almarcianita@gmail.com>
2018-03-16 17:02:06 -0600

    Pause on exit

--- src/pages/play-song/play-song.ts
+++ src/pages/play-song/play-song.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import {Song} from '../../models/songs/song.interface';
 // import {Media} from '@ionic-native/media';
 
@@ -18,8 +18,24 @@ export class PlaySongPage {
   media: any;
   song: Song;
   icon : String = "md-play";
+  timer:any;
+  time: number = 0;
+  
+  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private platform:Platform) {
+    platform.ready().then(() => {
+      this.platform.pause.subscribe(() => {
+        if(this.icon=="md-pause"){
+          this.media.pause();
+          this.icon == "md-play";
+
+        }
+
+
+      })
+    })
+
     
   }
 
@@ -29,6 +45,7 @@ export class PlaySongPage {
     
   }
 
+  
 
   loadSong(preview_url){
     this.media = new Media(preview_url);
@@ -38,8 +55,18 @@ export class PlaySongPage {
   playSong(){
       if(!this.media)
           this.loadSong(this.song.preview_url);
-        
-      
+
+          this.timer = setInterval(() => {
+            if(this.icon == "md-pause" ){
+              if(this.time >= 30){
+                this.time = 0;
+                this.playSong();
+              }else{
+                this.time++;
+              }
+            }               
+          }, 1000)
+                       
 
       if(this.icon == "md-play"){
             this.icon = "md-pause";
